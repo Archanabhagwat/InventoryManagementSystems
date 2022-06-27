8ab254ae4010240735d62ce8945f9f5e9ed621c0
SHRUTI MEDICALS <SHRUTI MEDICALS@Hp>
2022-06-28 00:19:20 +0530

    update

---
+++ InventoryManagementSystems/src/main/java/com/edu/InventoryManagementSystems/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.edu.InventoryManagementSystems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edu.InventoryManagementSystems.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+
+}
