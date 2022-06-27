f8da6797375b00e10d34c7c4f9e349fa15e3ac2b
SHRUTI MEDICALS <SHRUTI MEDICALS@Hp>
2022-06-28 00:15:53 +0530

    update

---
+++ InventoryManagementSystems/src/test/java/com/edu/InventoryManagementSystems/InventoryManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.InventoryManagementSystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
